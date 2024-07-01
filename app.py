80bbda99358773ef21b65f4ffe4514c61c7aae3b
acscoder-digitalonda <acscoder@digitalonda.com>
2024-07-01 15:40:39 +0700

    update

--- app.py
+++ app.py
@@ -169,7 +169,13 @@ if new_doc_modal.is_open():
         with tab1:
             doc_url = st.text_input("Enter your Gooogle Docs url:")
             submit_button = st.button("Submit")
-            if submit_button:
+            
+        with tab2:
+            vid_title = st.text_input("Youtube title:")
+            vid_url = st.text_input("Enter your Youtube url, Ex: https://www.youtube.com/watch?v=fflkFtIwQXo")
+            video_id = extract_youtube_id(vid_url)
+            submit_video = st.button("Submit")
+        if submit_button:
                 with st.spinner(text="Please patient,it may take some time to process the document."):
                     document_id,title,chunks = get_gdoc(doc_url)
                     if document_id in all_docs.keys():
@@ -180,12 +186,24 @@ if new_doc_modal.is_open():
                         save_doc_to_db(document_id,title)
                         st.session_state.all_docs = all_docs 
                         new_doc_modal.close()
-        with tab2:
-            vid_title = st.text_input("Youtube title:")
-            vid_url = st.text_input("Enter your Youtube url, Ex: https://www.youtube.com/watch?v=fflkFtIwQXo")
-            video_id = extract_youtube_id(vid_url)
-            submit_video = st.button("Submit")
-             
+        if submit_video:
+                with st.spinner(text="Please patient,it may take some time to process the document."):
+                    if not video_id or video_id in all_docs.keys():
+                        st.write("Video already exists.")
+                    else:            
+                        transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])  
+                        formatter = TextFormatter()
+                        formatted_transcript = formatter.format_transcript(transcript)
+                        
+                        save_doc_to_db(video_id,vid_title)
+                        all_docs[video_id] = vid_title
+                        
+                        tiktoken_encoding = tiktoken.get_encoding("cl100k_base")
+                        chunks = split_string_with_limit(formatted_transcript, 512,tiktoken_encoding)
+                        save_doc_to_vecdb(video_id,chunks)
+                        vid_title = ""
+                        vid_url = ""
+                        st.session_state.all_docs = all_docs        
 
 
 
