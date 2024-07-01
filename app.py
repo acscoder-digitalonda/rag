e7cfde6ca09d59d267621568ef74de20f4c75d48
acscoder-digitalonda <acscoder@digitalonda.com>
2024-07-01 18:46:26 +0700

    update

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def mongodb_client():
 def save_history_to_db(data):
     pid = data["_id"]
     sdt = []
-    for idx,dt in enumerate(data["history"]):\
+    for idx,dt in enumerate(data["history"]):
         dt["_id"] = str(pid)+"_"+str(idx)
         dt["pid"] = pid
         dt["order"] = idx
