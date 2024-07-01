ac29d1adcb930589242205ce6cf437d16d1bc39f
acscoder-digitalonda <acscoder@digitalonda.com>
2024-07-01 16:41:20 +0700

    update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from youtube_transcript_api.formatters import TextFormatter
 #from pinecone_text.dense import OpenAIEncoder
 #from pinecone_text.hybrid import hybrid_convex_scale
 from pymongo.mongo_client import MongoClient
- 
+import time
 import tiktoken
 from split_string import split_string_with_limit
 
@@ -45,42 +45,22 @@ data_index = pc.Index("chatdoc")
 
 model_name = "gpt-4-turbo-preview"
 def send_llm(prompt,data):
-    last_prompt = st.session_state.the_last_prompt
-    last_reply = st.session_state.the_last_reply
-    
-    system_prompting = "You are a helpful assistant."
-    if len(data):
-        system_prompting += "Based on these documents provided below, please complete the task requested by the user:" 
-        system_prompting += "\n\n".join(data)
-            
+    system_prompting,messages = get_llm_prompt(data)
     client = OpenAI(
         api_key=OPENAI_API_KEY,
     )
     our_sms = []
     our_sms.append({"role": "system", "content": system_prompting })
-    if last_prompt != "":
-        our_sms.append( {"role": "user", "content": last_prompt})
-    if last_reply != "":
-        our_sms.append( {"role": "assistant", "content": last_reply})
-    our_sms.append( {"role": "user", "content": prompt})
+    our_sms.extend(messages)
+     
     chat_completion = client.chat.completions.create(
         messages=our_sms,
         model=model_name,
     )
     return chat_completion.choices[0].message
 
-def send_llm_claude(prompt,data):
-    last_prompt = st.session_state.the_last_prompt
-    last_reply = st.session_state.the_last_reply
-    
-    system_prompting = "You are a helpful assistant."
-    if len(data):
-        system_prompting += "Based on these documents provided below, please complete the task requested by the user:" 
-        system_prompting += "\n\n".join(data)
-    our_sms = []
-     
-    our_sms.append( {"role": "user", "content": prompt})
-
+def send_llm_claude(data):
+    system_prompting,our_sms = get_llm_prompt(data)
     message = client_claude.messages.create(
     model="claude-3-5-sonnet-20240620",
     max_tokens=4096,
@@ -89,6 +69,17 @@ def send_llm_claude(prompt,data):
     )
     return message.content[0].text
 
+def get_llm_prompt(data):
+    if not system_prompt:
+        system_prompting = "You are a helpful assistant.Based on these documents provided below, please complete the task requested by the user:"
+    else:
+        system_prompting = system_prompt
+        system_prompting += "\n [CONTEXT] \n"
+        system_prompting += "\n\n".join(data)
+    
+    our_sms = st.session_state.chat_history["history"]
+    our_sms = our_sms[-10:]
+    return system_prompting,our_sms
 
 def add_to_index(data,nsp="default"):
     data_index.upsert(vectors=data,namespace=nsp)
@@ -209,11 +200,18 @@ if new_doc_modal.is_open():
                     st.write("Document added successfully.")
                     st.session_state.all_docs = all_docs        
 
-
+if not "chat_history" in st.session_state:
+    st.session_state.chat_history = {"id":int(time.time()),"history":[]}
 
 with st.sidebar:
   #st.subheader("Select Your Documents")  
   #doc_options = st.multiselect('Select the documents to query',all_docs.keys(),format_func = lambda x: all_docs[x] if x in all_docs else x,)
+  
+  system_prompt = st.text_area("System Prompt",
+                               '''You are an AI Assistant that help everyone by answering questions, and improve your answers from previous answers and CONTEXT information below.
+Answer in the same language the question was asked.Just reply directly, don't say base on history or mention about chat history.  
+If you don't know the answer, just say that you don't know.'''
+                               ) 
   api_option = st.selectbox(
     'Select the API',
     ('OpenAI', 'Anthropic'),
@@ -226,35 +224,25 @@ with st.sidebar:
   
   for doc_title in all_docs.values():
       st.text(doc_title)
-  
-if not "the_last_reply" in st.session_state:
-    st.session_state.the_last_reply = ""
-if not "the_last_prompt" in st.session_state:
-    st.session_state.the_last_prompt = ""
-        
-#your_prompt = st.text_area("Enter your Prompt:" ) 
+         
 your_prompt = st.chat_input ("Enter your Prompt:" ) 
-#submit_llm = st.button("Send")
 if your_prompt:
     #filter = get_filter_id([doc for doc in doc_options])
+    st.session_state.chat_history["history"].append({"role": "user", "content": your_prompt})
     data = get_from_index(your_prompt)
-    if len(data) > 0 :
-        data = cohere_rerank(your_prompt, data)
-        st.session_state.the_last_prompt = your_prompt
-        if api_option == "Anthropic" :
-            response = send_llm_claude(your_prompt,data)
-            st.session_state.the_last_reply = response
-            with st.chat_message("user"):
-                st.write(your_prompt)
-            with st.chat_message("assistant"):
-                st.write(response)
-        else:    
-            response = send_llm(your_prompt,data)
-            st.session_state.the_last_reply = response.content
-            
-            with st.chat_message("user"):
-                st.write(your_prompt)
-            with st.chat_message("assistant"):
-                st.write(response.content)
-    else:
-        st.write("Sorry, no documents selected.")
+    data = cohere_rerank(your_prompt, data)
+    
+    with st.chat_message("user"):
+        st.write(your_prompt)
+    if api_option == "Anthropic" :
+        response = send_llm_claude(data)
+        st.session_state.chat_history["history"].append({"role": "assistant", "content": response})    
+        with st.chat_message("assistant"):
+            st.write(response)
+    else:    
+        response = send_llm(data)
+        st.session_state.chat_history["history"].append({"role": "assistant", "content": response.content})
+        with st.chat_message("assistant"):
+            st.write(response.content)
+
+    #save chat_history  
