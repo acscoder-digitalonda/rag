fbeb66f02f84a11250466295fa42cd0f87100443
acscoder-digitalonda <acscoder@digitalonda.com>
2024-07-01 13:52:46 +0700

    update

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
- 
 from openai import OpenAI
 import streamlit as st
 from gdocs import gdocs
@@ -6,13 +5,28 @@ from streamlit_modal import Modal
 from pinecone import Pinecone
 import cohere
 from anthropic import Anthropic  
-
-
+import re
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+from pinecone_text.sparse import BM25Encoder
+from pinecone_text.dense import OpenAIEncoder
+from pinecone_text.hybrid import hybrid_convex_scale
+from pymongo.mongo_client import MongoClient
+from bs4 import BeautifulSoup 
+import requests 
+import tiktoken
+from split_string import split_string_with_limit
 
 ANTHROPIC_API_KEY = st.secrets['ANTHROPIC_API_KEY']   
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 PINECONE_API_KEY = st.secrets['PINECONE_API_KEY']
 COHERE_API_KEY = st.secrets['COHERE_API_KEY']
+MONGODB_API_KEY = st.secrets('MONGODB_API_KEY')
+MONGODB_API_APPNAME = st.secrets('MONGODB_API_APPNAME')
+#uri = f"mongodb+srv://{MONGODB_API_KEY}@cluster0.t7fr2hb.mongodb.net/?retryWrites=true&w=majority&appName={MONGODB_API_APPNAME}"
+
+BM25Encoder = BM25Encoder.default() 
+OpenAIEncoder = OpenAIEncoder() 
 
 cohere_client = cohere.Client(COHERE_API_KEY)
 def cohere_rerank(query: str,docs, top_n=3):
@@ -67,26 +81,19 @@ def send_llm_claude(prompt,data):
     our_sms.append( {"role": "user", "content": prompt})
 
     message = client_claude.messages.create(
-    model="claude-3-opus-20240229",
+    model="claude-3-5-sonnet-20240620",
     max_tokens=4096,
     system = system_prompting,
     messages=our_sms
     )
     return message.content[0].text
 
-embed_model = "text-embedding-3-large"
-def get_embedding(text ):
-   client = OpenAI(
-        api_key=OPENAI_API_KEY,
-    )
-   text = text.replace("\n", " ")
-   return client.embeddings.create(input = [text], model=embed_model).data[0].embedding
 
 def add_to_index(data,nsp="default"):
     data_index.upsert(vectors=data,namespace=nsp)
 
 def get_from_index(prompt,top_k=20,nsp="default",filter={}):
-    data = get_embedding(prompt)
+    data = get_query_embedding(prompt)
     res = data_index.query(vector=data,top_k=top_k,include_values=True,include_metadata=True,namespace=nsp,
                             filter=filter
                             )
@@ -104,7 +111,7 @@ def get_all_docs():
     
 def save_doc_to_db(document_id,title):
     metadata = {"doc_id": document_id,"text": title}
-    data = [{ "id": document_id, "values":get_embedding(title), "metadata": metadata}]
+    data = [{ "id": document_id, "values":get_document_embedding(title), "metadata": metadata}]
     add_to_index(data, "list")
 
 def save_doc_to_vecdb(document_id,chunks):
@@ -112,7 +119,7 @@ def save_doc_to_vecdb(document_id,chunks):
     lim = 100
     for idx,chunk in enumerate(chunks):
         metadata = {"doc_id": document_id,"text": chunk}
-        data.append({ "id": document_id+"_"+str(idx),"values":get_embedding(chunk),"metadata": metadata})
+        data.append({ "id": document_id+"_"+str(idx),"values":get_document_embedding(chunk),"metadata": metadata})
         if len(data) >= lim:
             add_to_index(data)
             data = []
@@ -126,6 +133,35 @@ def get_gdoc(url):
     chunks = gdocs.read_gdoc_content(creds,document_id)
     title = gdocs.read_gdoc_title(creds,document_id)
     return document_id,title,chunks
+
+def extract_youtube_id(url):
+    pattern = (
+        r'(?:https?://)?(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/|youtube\.com/embed/|youtube\.com/v/|youtube\.com/shorts/|youtube\.com/clip/|youtube\.com/user/.*#p/u/\d+/|youtube\.com/attribution_link\?a=|youtube\.com/.*#.*|youtube\.com/live/|youtube\.com/video/|youtube\.com/clip/|youtube\.com/.*#.*|youtube\.com/user/[^/]+/.*#.*|youtube\.com/[^/]+/[^/]+/[^/]+/|youtube\.com/.*\?v=|youtube\.com/.*\?clip_id=)([a-zA-Z0-9_-]{11})'
+    )
+    # Search for the pattern in the URL
+    match = re.search(pattern, url)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+def scrape_YT_title(url):      
+    r = requests.get(url) 
+    s = BeautifulSoup(r.text, "html.parser") 
+    title = s.find("span", class_="watch-title").text.replace("\n", "") 
+    return title 
+
+def get_query_embedding(text):
+    dense_vector = OpenAIEncoder.encode_queries([text])
+    sparse_vector = BM25Encoder.encode_queries(text)
+    hybrid_dense, _ = hybrid_convex_scale(dense_vector[0], sparse_vector, alpha=0.8)
+    return hybrid_dense
+
+def get_document_embedding(text):
+    dense_vector = OpenAIEncoder.encode_documents([text])
+    sparse_vector = BM25Encoder.encode_documents(text)
+    hybrid_dense, _ = hybrid_convex_scale(dense_vector[0], sparse_vector, alpha=0.8)
+    return hybrid_dense
 
 if not "all_docs" in st.session_state:
     st.session_state.all_docs = {}
@@ -141,37 +177,60 @@ new_doc_modal = Modal(
 )
 if new_doc_modal.is_open():
     with new_doc_modal.container():
-        doc_url = st.text_input("Enter your Gooogle Docs url:")
-        submit_button = st.button("Submit")
-        if submit_button:
-            with st.spinner(text="Please patient,it may take some time to process the document."):
-                document_id,title,chunks = get_gdoc(doc_url)
-                if document_id in all_docs.keys():
-                    st.write("Document already exists.")
-                else:
-                    all_docs[document_id] = title
-                    save_doc_to_vecdb(document_id,chunks)
-                    save_doc_to_db(document_id,title)
-                    st.session_state.all_docs = all_docs 
-                    new_doc_modal.close()
+        tab1, tab2 = st.tabs(["Gooogle Docs", "Youtube"])
+        with tab1:
+            doc_url = st.text_input("Enter your Gooogle Docs url:")
+            submit_button = st.button("Submit")
+            if submit_button:
+                with st.spinner(text="Please patient,it may take some time to process the document."):
+                    document_id,title,chunks = get_gdoc(doc_url)
+                    if document_id in all_docs.keys():
+                        st.write("Document already exists.")
+                    else:
+                        all_docs[document_id] = title
+                        save_doc_to_vecdb(document_id,chunks)
+                        save_doc_to_db(document_id,title)
+                        st.session_state.all_docs = all_docs 
+                        new_doc_modal.close()
+        with tab2:
+            vid_url = st.text_input("Enter your Youtube url:")
+            video_id = extract_youtube_id(vid_url)
+            if not video_id or video_id in all_docs.keys():
+                        st.write("Video already exists.")
+            else:            
+                transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])  
+                formatter = TextFormatter()
+                formatted_transcript = formatter.format_transcript(transcript)
+                title = scrape_YT_title(vid_url)
+                save_doc_to_db(video_id,title)
+                all_docs[video_id] = title
+                
+                tiktoken_encoding = tiktoken.get_encoding("cl100k_base")
+                chunks = split_string_with_limit(formatted_transcript, 512,tiktoken_encoding)
+                save_doc_to_vecdb(video_id,chunks)
+
+
 
 with st.sidebar:
   add_new_doc = st.button("Add New Document")
   if add_new_doc:
     new_doc_modal.open()
 
-  st.subheader("Select Your Documents")
+  '''st.subheader("Select Your Documents")
      
   doc_options = st.multiselect(
     'Select the documents to query',
     all_docs.keys(),
     format_func = lambda x: all_docs[x] if x in all_docs else x,
-    )
+    )'''
   api_option = st.selectbox(
     'Select the API',
     ('OpenAI', 'Anthropic'),
     )
-
+  st.subheader("Your Documents")
+  for doc_title in all_docs.keys():
+      st.text(doc_title)
+      st.divider()
   
 if not "the_last_reply" in st.session_state:
     st.session_state.the_last_reply = ""
@@ -182,8 +241,8 @@ if not "the_last_prompt" in st.session_state:
 your_prompt = st.chat_input ("Enter your Prompt:" ) 
 #submit_llm = st.button("Send")
 if your_prompt:
-    filter = get_filter_id([doc for doc in doc_options])
-    data = get_from_index(your_prompt,filter=filter)
+    #filter = get_filter_id([doc for doc in doc_options])
+    data = get_from_index(your_prompt)
     if len(data) > 0 :
         data = cohere_rerank(your_prompt, data)
         st.session_state.the_last_prompt = your_prompt
---
+++ local.py
@@ -0,0 +1,58 @@
+import os
+from dotenv import load_dotenv
+load_dotenv() 
+from pinecone_text.sparse import BM25Encoder
+from pinecone_text.dense import OpenAIEncoder
+from pinecone_text.hybrid import hybrid_convex_scale
+import re
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api.formatters import TextFormatter
+import tiktoken
+from pymongo.mongo_client import MongoClient
+from split_string import split_string_with_limit
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+MONGODB_API_KEY = os.environ.get('MONGODB_API_KEY')
+MONGODB_API_APPNAME = os.environ.get('MONGODB_API_APPNAME')
+
+uri = f"mongodb+srv://{MONGODB_API_KEY}@cluster0.t7fr2hb.mongodb.net/?retryWrites=true&w=majority&appName={MONGODB_API_APPNAME}"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+ 
+bm25 = BM25Encoder.default() 
+sparse_vector = bm25.encode_documents("The brown fox is quick")
+
+encoder = OpenAIEncoder()
+dense_vector = encoder.encode_queries(["Who jumped over the lazy dog?"])
+
+hybrid_dense, hybrid_sparse = hybrid_convex_scale(dense_vector[0], sparse_vector, alpha=0.8)
+ 
+
+def extract_youtube_id(url):
+    pattern = (
+        r'(?:https?://)?(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/|youtube\.com/embed/|youtube\.com/v/|youtube\.com/shorts/|youtube\.com/clip/|youtube\.com/user/.*#p/u/\d+/|youtube\.com/attribution_link\?a=|youtube\.com/.*#.*|youtube\.com/live/|youtube\.com/video/|youtube\.com/clip/|youtube\.com/.*#.*|youtube\.com/user/[^/]+/.*#.*|youtube\.com/[^/]+/[^/]+/[^/]+/|youtube\.com/.*\?v=|youtube\.com/.*\?clip_id=)([a-zA-Z0-9_-]{11})'
+    )
+    # Search for the pattern in the URL
+    match = re.search(pattern, url)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+video_url = "https://www.youtube.com/watch?v=dzmRE4h-FHo&t=22s" 
+video_id = extract_youtube_id(video_url)
+if video_id:
+    transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])  
+    formatter = TextFormatter()
+    formatted_transcript = formatter.format_transcript(transcript)
+    tiktoken_encoding = tiktoken.get_encoding("cl100k_base")
+    chunks = split_string_with_limit(formatted_transcript, 512,tiktoken_encoding)
+    print(chunks[0])
